409316a7014ecfbb5d52ac58a45028e41a0bb7a2
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2024-01-08 16:34:21 +0700

    fix/login

--- src/features/services/authentication/authentication-api.ts
+++ src/features/services/authentication/authentication-api.ts
@@ -1,14 +1,20 @@
 import axios from "axios";
 
 
-export type postLoginPayload = { 
+export type postLoginPayload = {
     email: string;
     password: string;
 }
 
 
 const postLogin = (payload: postLoginPayload) => {
-    return axios.post('login', payload);
+    return axios.get("sanctum/csrf-cookie", {
+        baseURL: "https://imtalabs.tech/"
+    }).then(() => {
+        console.log(payload);
+
+        return axios.post('login', payload)
+    })
 }
 
 const postLogout = () => {
